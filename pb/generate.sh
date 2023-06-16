3547d9e3d9830e4c974a93761bf73c49294e2aba
colindickson <colin.dickson@gmail.com>
2023-06-16 07:52:11 -0400

    use buf for proto generation

--- pb/generate.sh
+++ pb/generate.sh
@@ -1,61 +1,61 @@
-#!/bin/bash
+#!/bin/bash -u
+# Copyright 2019 dfuse Platform Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 # Protobuf definitions
 PROTO=${1:-"$ROOT/proto"}
 
-
 function main() {
   checks
 
   set -e
+  pushd "$ROOT" >/dev/null
+
+  generate_proto
+
+  popd >/dev/null
 
 
-  pushd "$ROOT/pb" &> /dev/null
+  echo "generate.sh - `date` - `whoami`" > $ROOT/pb/last_generate.txt
+  echo "streamingfast/proto revision: `GIT_DIR=$ROOT/.git git rev-parse HEAD`" >> $ROOT/pb/last_generate.txt
 
-  generate "sf/authentication/v1/authentication.proto"
-
-  echo "generate.sh - `date` - `whoami`" > ./last_generate.txt
-  echo "adminadpi/proto revision: `GIT_DIR=$ROOT/.git git rev-parse HEAD`" >> ./last_generate.txt
+  echo "Done"
 }
 
-# usage:
-# - generate <protoPath>
-# - generate <protoBasePath/> [<file.proto> ...]
-function generate() {
-    base=""
-    if [[ "$#" -gt 1 ]]; then
-      base="$1"; shift
-    fi
-
-    for file in "$@"; do
-      protoc -I$PROTO \
-        --go_out=. --go_opt=paths=source_relative \
-        --go-grpc_out=. --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false \
-         $base$file
-    done
+function generate_proto() {
+  echo "Generating Substreams Protobuf bindings via 'buf'"
+  buf generate proto
 }
 
 function checks() {
-  # The old `protoc-gen-go` did not accept any flags. Just using `protoc-gen-go --version` in this
-  # version waits forever. So we pipe some wrong input to make it exit fast. This in the new version
-  # which supports `--version` correctly print the version anyway and discard the standard input
-  # so it's good with both version.
-  result=`printf "" | protoc-gen-go --version 2>&1 | grep -Eo v[0-9\.]+`
+  result=`printf "" | buf --version 2>&1 | grep -Eo '1\.(1[0-9]+|[2-9][0-9]+)\.'`
   if [[ "$result" == "" ]]; then
-    echo "Your version of 'protoc-gen-go' (at `which protoc-gen-go`) is not recent enough."
+    echo "The 'buf' binary is either missing or is not recent enough (at `which buf || echo N/A`)."
     echo ""
-    echo "To fix your problem, perform those commands:"
+    echo "To fix your problem, on Mac OS, perform this command:"
     echo ""
-    echo "  go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0"
-    echo "  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0"
+    echo "  brew install bufbuild/buf/buf"
+    echo ""
+    echo "On other system, refers to https://docs.buf.build/installation"
     echo ""
     echo "If everything is working as expetcted, the command:"
     echo ""
-    echo "  protoc-gen-go --version"
+    echo "  buf --version"
     echo ""
-    echo "Should print 'protoc-gen-go v1.26.0' (if it just hangs, you don't have the correct version)"
+    echo "Should print '1.11.0' (or newer)"
     exit 1
   fi
 }
