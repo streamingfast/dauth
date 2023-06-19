e1513ae62c89c6df8f16cf08a415f1fa32a46c30
Julien Cassis <juliencassis@gmail.com>
2023-06-19 13:06:48 -0400

    Added dauth FromContext helper

--- headers.go
+++ headers.go
@@ -1,7 +1,31 @@
 package dauth
 
-const (
-	SFHeaderUserID   string = "X-Sf-User-Id"
-	SFHeaderApiKeyID string = "X-Sf-Api-Key-Id"
-	SFHeaderIP       string = "X-Real-Ip"
+import (
+	"context"
+	"google.golang.org/grpc/metadata"
 )
+
+const (
+	SFHeaderUserID   string = "x-sf-user-id"
+	SFHeaderApiKeyID string = "x-sf-api-key-id"
+	SFHeaderIP       string = "x-real-ip"
+)
+
+func FromContext(ctx context.Context) (userID string, apiKeyId string, ip string) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return
+	}
+
+	userID = getHeader(SFHeaderUserID, md)
+	apiKeyId = getHeader(SFHeaderApiKeyID, md)
+	ip = getHeader(SFHeaderIP, md)
+	return
+}
+
+func getHeader(key string, md metadata.MD) string {
+	if len(md.Get(key)) > 0 {
+		return md.Get(key)[0]
+	}
+	return ""
+}
